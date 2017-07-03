2af1b0f31e8c4ffe55dbc8771d2e85050751363e
Ling Jin <chloejin525@gmail.com>
2017-07-03 10:38:24 -0400

    initialization

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'welcome/home'
+
+  get 'welcome/contact'
+
+  get 'welcome/about'
+
+  resources :activities
+  resources :states
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#home'
+end
